fffdcb61147b166862088e3b95d41c981af497fb
Siva V <sivavijayakumar1222@gmail.com>
2025-02-05 16:34:57 +0530

    modified 5 files

--- Frontend/src/components/ChatContainer.jsx
+++ Frontend/src/components/ChatContainer.jsx
@@ -1,5 +1,6 @@
 import { useChatStore } from "../store/useChatStore";
 import { useEffect, useRef } from "react";
+
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
@@ -7,54 +8,71 @@ import { useAuthStore } from "../store/useAuthStore";
 import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
-    const {messages, getMessages, isMessagesLoading, selectedUser, subscribeToMessages, unsubscribeFromMessages} = useChatStore();
-    const {authUser} = useAuthStore();
-    const messageEndRef = useRef(null);
+  const {
+    messages,
+    getMessages,
+    isMessagesLoading,
+    selectedUser,
+    subscribeToMessages,
+    unsubscribeFromMessages,
+  } = useChatStore();
+  const { authUser } = useAuthStore();
+  const messageEndRef = useRef(null);
 
-    useEffect(() => {
-        getMessages(selectedUser._id);
-        subscribeToMessages();
+  useEffect(() => {
+    getMessages(selectedUser._id);
 
-        return () => unsubscribeFromMessages();
-    }, [selectedUser._id, getMessages, subscribeToMessages, unsubscribeFromMessages]);
+    subscribeToMessages();
 
-    useEffect(()=>{
-        if(messageEndRef.current && messages){
-            messageEndRef.current.scrollIntoView({behavior: "smooth"});
-        }
-    }, [messages])
+    return () => unsubscribeFromMessages();
+  }, [selectedUser._id, getMessages, subscribeToMessages, unsubscribeFromMessages]);
 
-    if(isMessagesLoading) {
-        return (
-        <div className="flex-1 flex flex-col overflow-auto">
-            <ChatHeader/>
-            <MessageSkeleton />
-            <MessageInput />
-        </div>
-    );
-}
+  useEffect(() => {
+    if (messageEndRef.current && messages) {
+      messageEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
+
+  if (isMessagesLoading) {
     return (
-        <div className="flex-1 flex flex-col overflow-auto">
-            <ChatHeader/>
-            <div className="flex-1 overflow-auto px-4 space-y-4">
-                {messages.map((message) => (
-                    <div
-                        key={message._id}
-                        className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
-                        ref={messageEndRef}
-                    >
-                        <div className="chat-image avatar">
-                            <div className="size-10 rounded-full border">
-                                <img src={message.senderId === authUser._id ? authUser.profilePic || "/avatar.jpg" : selectedUser.profilePic || "/avatar.jpg"} alt="ProfilePic" />
-                            </div>
-                        </div>
-                    <div className="chat-header mb-1"> 
-                        <time className="text-xs opacity-50 ml-1">
-                            {formatMessageTime(message.createdAt)}
-                        </time>
-                    </div>
-                    <div className="chat-bubble flex flex-col">
-                    {message.image && (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+        {messages.map((message) => (
+          <div
+            key={message._id}
+            className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+            ref={messageEndRef}
+          >
+            <div className=" chat-image avatar">
+              <div className="size-10 rounded-full border">
+                <img
+                  src={
+                    message.senderId === authUser._id
+                      ? authUser.profilePic || "/avatar.jpg"
+                      : selectedUser.profilePic || "/avatar.jpg"
+                  }
+                  alt="profile pic"
+                />
+              </div>
+            </div>
+            <div className="chat-header mb-1">
+              <time className="text-xs opacity-50 ml-1">
+                {formatMessageTime(message.createdAt)}
+              </time>
+            </div>
+            <div className="chat-bubble flex flex-col">
+              {message.image && (
                 <img
                   src={message.image}
                   alt="Attachment"
@@ -62,14 +80,13 @@ const ChatContainer = () => {
                 />
               )}
               {message.text && <p>{message.text}</p>}
-                    </div>
-                    </div>
-                ))}
             </div>
+          </div>
+        ))}
+      </div>
 
-            <MessageInput />
-        </div>
-    );
+      <MessageInput />
+    </div>
+  );
 };
-
 export default ChatContainer;
