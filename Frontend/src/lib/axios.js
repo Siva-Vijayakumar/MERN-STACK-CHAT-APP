ff42a773aa494b9344b79f6feaf67099f70b60e0
Siva V <sivavijayakumar1222@gmail.com>
2025-02-05 17:17:37 +0530

    FrontendProjectChatApp

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? 'http://localhost:5001/api' : "/api",
+    baseURL: 'https://backend-chatapp-glm2.onrender.com/api',
     withCredentials: true,
 });
+
+// import axios from 'axios';
+
+// export const axiosInstance = axios.create({
+//     baseURL: import.meta.env.MODE === "development" ? 'http://localhost:5001/api' : "/api",
+//     withCredentials: true,
+// });
