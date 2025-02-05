305f2ee05d059766037ce624b85daa4ebf3bc990
Siva V <sivavijayakumar1222@gmail.com>
2025-02-05 16:45:31 +0530

    All 5 file changes Reversed

--- Frontend/src/pages/SettingsPage.jsx
+++ Frontend/src/pages/SettingsPage.jsx
@@ -1,14 +1,15 @@
-import { THEMES } from "../constants";
-import { useThemeStore } from "../store/useThemeStore";
-import { Send } from "lucide-react";
+import React from 'react'
+import { THEMES } from '../constants';
+import { Send } from 'lucide-react';
+import { useThemeStore } from '../store/useThemeStore';
 
 const PREVIEW_MESSAGES = [
-  { id: 1, content: "Hey! How's it going?", isSent: false },
-  { id: 2, content: "I'm doing great! Just working on some new features.", isSent: true },
+  { id: 1, text: "Hey! How's it going ?", isSent:false },
+  { id: 2, text: "I'm doing great! Just Working on some new features.", isSent:true },
 ];
 
 const SettingsPage = () => {
-  const { theme, setTheme } = useThemeStore();
+  const {theme, setTheme} = useThemeStore();
 
   return (
     <div className="h-screen container mx-auto px-4 pt-20 max-w-5xl">
@@ -57,7 +58,7 @@ const SettingsPage = () => {
                       J
                     </div>
                     <div>
-                      <h3 className="font-medium text-sm">John Doe</h3>
+                      <h3 className="font-medium text-sm">Mike</h3>
                       <p className="text-xs text-base-content/70">Online</p>
                     </div>
                   </div>
@@ -76,14 +77,14 @@ const SettingsPage = () => {
                           ${message.isSent ? "bg-primary text-primary-content" : "bg-base-200"}
                         `}
                       >
-                        <p className="text-sm">{message.content}</p>
+                        <p className="text-sm">{message.text}</p>
                         <p
                           className={`
                             text-[10px] mt-1.5
                             ${message.isSent ? "text-primary-content/70" : "text-base-content/70"}
                           `}
                         >
-                          12:00 PM
+                          05:28 PM
                         </p>
                       </div>
                     </div>
@@ -112,5 +113,6 @@ const SettingsPage = () => {
       </div>
     </div>
   );
-};
-export default SettingsPage;
+}
+
+export default SettingsPage
