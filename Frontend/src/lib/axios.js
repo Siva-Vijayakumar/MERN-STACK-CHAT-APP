cd86c71a2e81f3d595f03cf91270621c43409a84
Siva-Vijayakumar <167623516+Siva-Vijayakumar@users.noreply.github.com>
2025-02-05 17:54:38 +0530

    Update axios.js

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,13 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://backend-chatapp-glm2.onrender.com/api',
+    baseURL: import.meta.env.MODE === "development" ? 'http://localhost:5001/api' : "/api",
     withCredentials: true,
 });
-
-// import axios from 'axios';
-
-// export const axiosInstance = axios.create({
-//     baseURL: import.meta.env.MODE === "development" ? 'http://localhost:5001/api' : "/api",
-//     withCredentials: true,
-// });
