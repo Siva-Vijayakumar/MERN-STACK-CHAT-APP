159d6b1e5e642b49a42d69bcb99f45cb9bd8ddd0
Siva V <sivavijayakumar1222@gmail.com>
2025-02-06 08:41:59 +0530

    final commit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,43 +1,44 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.route.js";
-import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
-import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
-import { app, server } from "./lib/socket.js";
+
 import path from "path";
 
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
+
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: '50mb' }));
-app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json());
 app.use(cookieParser());
-
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../Frontend/dist")));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
-    });
-}
-
-app.use(cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-}));
-
-
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-server.listen(PORT, ()=>{
-    console.log("server is running on port", PORT);
-    connectDB();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
